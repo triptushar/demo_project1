5029150ab2dd84f6040dcb8dcd22e94f42543297
Tushar Saxena <livefasterthanlife@gmail.com>
2015-01-31 22:20:27 +0530

    modified gitull.php for correct path

--- gitpull.php
+++ gitpull.php
@@ -1,4 +1,4 @@
 <?php
-$output = shell_exec('cd /var/www/html/api/dev/demo_project1/;git pull');
+$output = shell_exec('cd /var/www/html/dev/demo_project1/;git pull');
 echo "<pre>$output</pre>";
 ?>
